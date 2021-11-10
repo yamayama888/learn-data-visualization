80e5fa5b10eef722711b9a3285f466c7a3a577f6
yamayama888 <gishi916yama@gmail.com>
2021-11-10 17:21:16 +0900

    ダミーデータをうんたら

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
+import embed from 'vega-embed';
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const spec = {
+  $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+  width: 360,
+  data: {
+    values: [
+      {a: 'A', b: 28}, {a: 'B', b: 55}, {a: 'C', b: 43},
+      {a: 'D', b: 91}, {a: 'E', b: 81}, {a: 'F', b: 53},
+      {a: 'G', b: 19}, {a: 'H', b: 87}, {a: 'I', b: 52}
+    ],
+    name: 'source'
+  },
+  selection: {
+    a: {type: 'single'}
+  },
+  mark: 'bar',
+  encoding: {
+    x: {field: 'a', type: 'ordinal', axis: {labelAngle: 0}},
+    y: {field: 'b', type: 'quantitative'},
+    tooltip: {field: 'b', type: 'quantitative'},
+    color: {
+      condition: {selection: 'a', value: 'steelblue'},
+      value: 'grey'
+    }
+  },
+  // pass options to Vega-Embed
+  usermeta: {
+    embedOptions: {
+      renderer: 'svg'
+    }
+  }
+};
+
+embed("#app", spec);
